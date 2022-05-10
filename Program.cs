66a46550ffc4ea9676394a2cab507333733b240c
codejuanma <105258974+codejuanma@users.noreply.github.com>
2022-05-10 02:09:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// solicitar un numero al cliente y si el numero es 1000 imprimir ganaste un premio
+
+Console.WriteLine("Ingrese su numero por favor");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero == 1000)
+    Console.WriteLine("ganaste un premio");
+else if (numero != 1000)
+    Console.WriteLine("suerte para la proxima");
